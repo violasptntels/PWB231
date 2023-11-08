564c33b2869c5165ac82cfee29170b44bc01f7c8
FARAZIFTAN <mahasiswaulbiit@gmail.com>
2023-11-09 01:59:09 +0700

    Tugas JS Rajif

---
+++ Chapter06/tugas/714230064_A.M. Faraziftan_JS/script.js
@@ -0,0 +1,25 @@
+function Paragrafchangeimagechange() {
+    document.getElementById("desc").innerHTML =
+    "Tailwind CSS dibuat oleh Adam Wathan" +
+    "\r\n" +
+    "sekarang menjabat sebagai CEO Tailwind Labs.";
+    document.getElementById("image").src = "";
+}
+function Statement() {
+    var a, b, c;
+    a= 21;
+    b= 2;
+    c= a+b;
+    alert(
+        "Sejauh ini yang saya ketahui mahasiswa D4 Teknik Informatika 1B pengguna Tailwind CSS sekitar " +
+        c +
+        " Pengguna"
+    );
+}
+function AritmatikaSyntax() {
+    let a = 2;
+    let b = 1;
+
+    let tambah = a + b;
+    alert("Tailwind CSS merupakan pembelajaran ke " + tambah + " Pemograman");
+}
