ca015713e295b571b0a6c070e7d70bb5ebe32e6a
mrestuab <mrestuab54@gmail.com>
2023-11-08 23:37:42 +0700

    ini tugas chapter 6 restu pak

---
+++ Chapter06/tugas/714230059 moch restu/script.js
@@ -0,0 +1,24 @@
+function ParagrafChangeImageChange() {
+    document.getElementById("caption").innerHTML =
+    "ayo beli! supaya kami punya duit, " +
+    "" +
+    " mumpung masi murah gais ayo." ;
+
+    document.getElementById("image").src = "baju2.png" ;
+}
+
+function Statement() {
+    var a, b, c;
+    a = 2;
+    b = 1;
+    c = a * b;
+    alert("kami baru mempunyai " + c + " produk");
+}
+
+function AritmatikaSyntax() {
+    let a = 2;
+    let b = 3;
+    let tambah = a + b;
+
+    alert("cuma " + tambah + " baju lagi, buruan beli keburu habis");
+}
--- Chapter06/tugas/README.md
+++ Chapter06/tugas/README.md
@@ -1,7 +1,2 @@
-# Tugas Praktikum
-1. Buat Folder NPM_NAMA_JS
-2. Buatkan 1 tampilan website sederhana yang yang sudah dikerjakan sebelumnya
-3. menggunakan HTML dan CSS Tailwind
-4. Aritmatik, Syntax, Statement, Image Change, Button, Penggantian Paragraf dari Button
-5. script.js eksternal
-6. Buatkan Readme.md sesuai Studi kasus masing-masing
+saat tekan "click here" maka paragraf dan poto bajunya akan berubah
+dan pada saat menekan "product" dan "ready stock" maka akan keluar aritmatikanya
