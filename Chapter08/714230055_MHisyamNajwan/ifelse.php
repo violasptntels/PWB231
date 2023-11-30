33c4dd2de543f23323158e79dee7865c2e293b89
HisyamSamAm <mhisyamnajwan@gmail.com>
2023-11-30 22:30:30 +0700

    Praktikum PHP MHisyamNajwan

---
+++ Chapter08/714230055_MHisyamNajwan/ifelse.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Penggunaan Kontrol if</h2>
+    <?php
+    $umur=15;
+    echo("Umur anda $umur tahun <br>");
+    if ($umur>=17)
+    {
+        echo("Anda Boleh Masuk");
+    }else
+    {
+        echo("Anda tidak boleh masuk");
+    }
+    ?>
+</body>
+</html>
