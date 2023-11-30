bd718ffde28c93cb57c0f72688bd220588081b0b
hilmibotak <hilmiromadoni@gmail.com>
2023-11-30 23:31:52 +0700

    chapter 8

---
+++ Chapter08/714230057_hilmi/komentar.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Penulisan Komentar PHP</title>
+</head>
+<body>
+    <h2>Struktur penulisan PHP</h2>
+    <?php
+    echo("Selamat datang di kawasan PHP");
+    ?>
+</body>
+</html>
