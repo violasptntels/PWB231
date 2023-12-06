95259889a1045a5ab0cf79ee60dd60f0f2d25134
Fadhail <mochammadfadhail2@gmail.com>
2023-12-06 21:55:13 +0700

    Tugas PHP Fadhail

---
+++ Chapter08/TugasDirumah/714230044_Fadhail/Latihan1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+<h2>Latihan 1</h2>
+<?php
+$bil=714230044;
+
+if ($bil % 2 == 0)
+{ 
+    echo ("$bil Merupakan Bilangan Genap");
+}
+else 
+{
+    echo ("$bil Merupakan Bilangan Ganjil");
+}
+?>
+</body>
+</html>
---
+++ Chapter08/TugasDirumah/714230044_Fadhail/Latihan2.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Latihan 2</title>
+</head>
+<body>
+<h2>Latihan 2</h2>
+<?php 
+    $jumlahGenap = 0;
+
+    for ($i = 0; $i <= 6; $i += 2) {
+        $jumlahGenap += $i;
+    }
+    echo ("Jumlah bilangan genap dari 0 sampai 6 adalah $jumlahGenap");
+?>
+</body>
+</html>
