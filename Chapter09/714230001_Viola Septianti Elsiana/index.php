ab502cbad02afdce4e8541343a744cf2a3e74af0
violasptntels <violaseptiantiana23@gmail.com>
2023-12-07 18:52:56 +0700

    tugas PHP hari ini

---
+++ Chapter09/714230001_Viola Septianti Elsiana/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran Mahasiswa Baru</title>
+</head>
+<body>
+    <header>
+        <h3>Pendaftaran Mahasiswa</h3>
+        <h3>ULBI</h3>
+    </header>
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar</a></li>
+            <li><a href="list-maba.php">Pendatar</a></li>
+        </ul>
+    </nav>
+    <h3>Pendaftaran Mahasiswa Baru ULBI Berhasil!!!</h3>
+</body>
+</html>
